cad62906bbdc285aac2e2f6903c99ceb84c27c17
louisbutt338 <louisbutt338@gmail.com>
2025-07-29 17:21:14 +0100

    updates to printing

--- read_current.py
+++ read_current.py
@@ -1,5 +1,6 @@
 import time
 import serial
+import threading
 
 # configure the serial connections (the parameters differ on the device you are connecting to)
 try:
@@ -15,32 +16,46 @@ try:
 except OSError:
       print("Error connecting device")
       exit()
+#open the serial connection
 ser.isOpen()
 
 # print the instrument description
-#ser.write(b'*idn?\r')
-#serial_line = ser.readline()
-#print(serial_line)
+def instrument_info():
+    ser.write(b'*idn?\r')
+    serial_line = ser.readline()
+    print(serial_line)
 
 # do zero correction for current readings
-ser.write(b'*rst \r')
-ser.write(b'syst:zch on \r ; rang 2e-9  \r ; init \r ')
-ser.write(b'syst:zcor:acq \r ; syst:zcor on \r ; rang:auto on \r ')
-ser.write(b'syst:zch off \r ')
+def zero_correction():
+    ser.write(b'*rst \r')
+    ser.write(b'syst:zch on \r ; rang 200e-6  \r ; init \r ')
+    ser.write(b'syst:zcor:acq \r ; syst:zcor on \r ; rang:auto on \r ')
+    ser.write(b'syst:zch off \r ')
 
-# Reading the data from the serial port. This will be running in an infinite loop.
+# remote control the keithley and trigger current measurements at time intervals 
+def read_data():
+    ser.write(b'read? \r')
+    serial_line = ser.readline()
+    print('current:',serial_line[:41], 'time:',time.ctime())
+
+# zero correct
+zero_correction()
+
+# set time interval (s) between each Keithley readings (minimum 1.4s due to legnth of read_data fn)
+time_interval = 2
+time.sleep(time_interval)
+
+# infinite loop to read the data from the serial port
 while True :
+        # take measurement and measure time taken
         st = time.time()
-        # remote control the keithley and trigger zero-corr current measurements at time intervals 
-        ser.write(b'read? \r')
-        serial_line = ser.readline()
-        print('current:',serial_line[:-1], 'time:',time.ctime())
+        read_data()
         et=time.time()
         loop_time = et-st
-        # time for program to run is ~1.38s. sleep for remainder of set time interval
-        time.sleep(2-loop_time)
-
-        # for reading and printing the bytes in waiting
-        #bytesToRead = ser.in_waiting
-        #data = ser.read(bytesToRead)
-        #print(data)
+        
+        # sleep for remainder of set time interval, or nothing
+        read_time = et-st
+        if time_interval > read_time:
+            time.sleep(time_interval-read_time)
+        else:
+            time.sleep(0)
